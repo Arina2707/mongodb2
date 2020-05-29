7b9af9488f1adf2cb0c8344f5109bb6008d92697
Arina <56595596+Arina2707@users.noreply.github.com>
2020-05-29 21:55:00 +0300

    Update connection.py

--- connection.py
+++ connection.py
@@ -2,7 +2,7 @@ from mongoengine import *
 
 
 def cluster_connection():
-    cluster_uri = "mongodb+srv://Arina:arina270799@cluster27-pldc3.gcp.mongodb.net/cluster_new?retryWrites=true&w=1"
+    cluster_uri = "mongodb+srv://@pldc3.gcp.mongodb.net/cluster_new?retryWrites=true&w=1"
     conn = connect("cluster_new", host=cluster_uri)
     print("Successfully connected!")
     mydb = conn['cluster_new']
